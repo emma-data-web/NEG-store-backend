94d8a1885691156ca6b5a1b546f22488dc260216
emma-data-web <emmanuelnwankwo690@gmail.com>
2025-12-11 21:22:50 +0100

    fixed update store

--- app/routers/store.py
+++ app/routers/store.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from app.schemas.auth import Message
 from app.dependencies import get_db
-from app.schemas.store import StoreResponse, CreateStore
+from app.schemas.store import StoreResponse, CreateStore, StoreUpdate
 from app.services.store_services import create_store, update_store
 from app.services.user_services import get_current_user
 from app.models.user import User
@@ -17,3 +17,15 @@ def store(data : CreateStore,
   new_store =create_store(db=db, data=data, current_user =current_user)
 
   return new_store
+
+
+
+@store_router.put('/update-store/{store_id}')
+def store_update(
+    store_id: int,
+    data: StoreUpdate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    updated_store = update_store(db, store_id, data, current_user)
+    return updated_store
