baa08d519a040c37619e25bc73e68631a6f2fee2
emma-data-web <emmanuelnwankwo690@gmail.com>
2026-07-25 11:58:02 +0100

    fixed the caching issues on product

--- app/services/product_services.py
+++ app/services/product_services.py
@@ -4,6 +4,8 @@ from app.schemas.product import ProductCreate, ProductUpdate
 from app.models.user import User
 from app.models.product import Product
 from fastapi import HTTPException
+import json
+from app.utils.redis_client import client
 
 
 def create_product(db: Session, data: ProductCreate, current_user: User):
@@ -27,10 +29,34 @@ def create_product(db: Session, data: ProductCreate, current_user: User):
 
 
 def get_product_by_id(db: Session, product_id: int):
+
+  cache_key = f"product:{product_id}"
+
+  cached_product = client.get(cache_key)
+
+  
+  if cached_product:
+    return json.loads(cached_product)
+
   product = db.query(Product).filter(Product.id == product_id).first()
 
   if not product:
     raise HTTPException(status_code=404, detail="Product not found")
+
+  product_data = {
+    "name": product.name,
+    "price": float(product.price),
+    "description": product.description,
+    "quantity": product.quantity,
+    "category_id": product.category_id,
+    "image_url": product.image_url
+  }
+
+  client.setex(
+    cache_key,           
+    300,                
+    json.dumps(product_data)  
+)
   
-  return product
+  return product_data
 
---
+++ app/utils/redis_client.py
@@ -0,0 +1,10 @@
+import redis
+
+client = redis.Redis(
+
+    host="localhost",
+    port=6379,
+    decode_responses=True
+
+)
+
